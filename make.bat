b2ce05a993077347219ce59456f71d81d69d96bc
martin_cerny <cerny.m@gmail.com>
2016-11-15 13:35:03 +0100

    Fixed the make script

--- make.bat
+++ make.bat
@@ -1,6 +1,7 @@
-elm-make src\Main.js --output bin\main.js
+elm-make src\Main.elm --output bin\main.js
 
 if not exist bin\img mkdir bin\img 
-xcopy img bin\img /s /e
+xcopy img bin\img /s /e /y
 
+copy *.css bin
 copy index-dist.html bin\index.html
